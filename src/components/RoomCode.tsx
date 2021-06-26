4572eb6a51b9ec08b11cec4a2b4ce334f24b8c85
gabriel-fig-2020 <gabrielfgsantos95@gmail.com>
2021-06-25 23:54:53 -0300

    final nlw 6

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,12 +1,12 @@
 import copyImg from '../assets/images/copy.svg';
 
-import '../styles/room-code.scss'
+import '../styles/room-code.scss';
 
 type RoomCodeProps = {
   code: string;
-}
-export function RoomCode(props: RoomCodeProps) {
+} 
 
+export function RoomCode(props: RoomCodeProps) {
   function copyRoomCodeToClipboard() {
     navigator.clipboard.writeText(props.code)
   }
@@ -16,8 +16,7 @@ export function RoomCode(props: RoomCodeProps) {
       <div>
         <img src={copyImg} alt="Copy room code" />
       </div>
-      <span >Sala #{props.code}</span>
+      <span>Sala #{props.code}</span>
     </button>
-
   )
 }
